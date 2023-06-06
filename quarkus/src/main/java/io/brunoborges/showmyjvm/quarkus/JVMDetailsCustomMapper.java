cb0b7b3a9591c47bb19887245e5ee7345cec2663
Georgios Andrianakis <geoand@gmail.com>
2023-06-06 07:48:31 +0300

    Use RESTEasy Reactive in Quarkus application
    RESTEasy Reactive is the now the standard
    REST API layer in Quarkus so best to use it

--- quarkus/src/main/java/io/brunoborges/showmyjvm/quarkus/JVMDetailsCustomMapper.java
+++ quarkus/src/main/java/io/brunoborges/showmyjvm/quarkus/JVMDetailsCustomMapper.java
@@ -1,8 +1,12 @@
 package io.brunoborges.showmyjvm.quarkus;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.brunoborges.showmyjvm.core.JVMDetails;
+import io.quarkus.jackson.JacksonMixin;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 import jakarta.inject.Singleton;
 
@@ -10,11 +14,17 @@ import jakarta.inject.Singleton;
 public class JVMDetailsCustomMapper implements ObjectMapperCustomizer {
 
     @Override
-    public void customize(ObjectMapper objectMapper) {
-        objectMapper.addMixIn(JVMDetails.class, FilterMemoryPool.class);
-        // objectMapper.setDefaultPrettyPrinter(new DefaultPrettyPrinter());
+    public void customize(ObjectMapper om) {
+        om.enable(SerializationFeature.INDENT_OUTPUT);
+        om.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+
+        DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+        prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+
+        om.setDefaultPrettyPrinter(prettyPrinter);
     }
 
+    @JacksonMixin(JVMDetails.class)
     @JsonIgnoreProperties(value = {"memoryPoolMXBeans"})
     public static class FilterMemoryPool {
     }
--- quarkus/src/main/java/io/brunoborges/showmyjvm/quarkus/ShowMyJVMResource.java
+++ quarkus/src/main/java/io/brunoborges/showmyjvm/quarkus/ShowMyJVMResource.java
@@ -1,28 +1,21 @@
 package io.brunoborges.showmyjvm.quarkus;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.jakarta.rs.annotation.JacksonFeatures;
 import io.brunoborges.showmyjvm.core.JVMDetails;
 import io.brunoborges.showmyjvm.core.ShowJVM;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 
 @Path("/jvm")
 public class ShowMyJVMResource {
 
     @GET
     @Path("/inspect")
-    @Produces(MediaType.TEXT_PLAIN)
     public String showmyjvm() {
         return new ShowJVM().dumpJVMDetails();
     }
 
     @GET
     @Path("/inspect.json")
-    @Produces(MediaType.APPLICATION_JSON)
-    @JacksonFeatures(serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
     public JVMDetails jvmdetails() {
         return new ShowJVM().extractJVMDetails();
     }
