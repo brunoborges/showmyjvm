052212c5bfb59f45ce819b8fb17d50fbe2fe4719
Bruno Borges <brborges@microsoft.com>
2025-02-13 10:58:39 -0800

    Update settings

--- core/src/main/java/io/brunoborges/showmyjvm/core/ShowJVM.java
+++ core/src/main/java/io/brunoborges/showmyjvm/core/ShowJVM.java
@@ -4,12 +4,7 @@ import java.lang.management.ManagementFactory;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ShowJVM {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ShowJVM.class);
 
     public static void main(String[] args) {
         System.out.println(new ShowJVM().dumpJVMDetails());
