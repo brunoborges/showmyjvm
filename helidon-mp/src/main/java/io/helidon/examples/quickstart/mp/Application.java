dc5c9f2d4755717655baa257e25c724dd1458626
Bruno Borges <brborges@microsoft.com>
2025-12-11 22:45:41 -0800

    Update POM files and application properties for Helidon and Tomcat; add main class and dependency management

---
+++ helidon-mp/src/main/java/io/helidon/examples/quickstart/mp/Application.java
@@ -0,0 +1,9 @@
+package io.helidon.examples.quickstart.mp;
+
+public class Application {
+
+    public static void main(String[] args) {
+        io.helidon.microprofile.cdi.Main.main(args);
+    }
+
+}
